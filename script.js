e2f9b4ff84e7b30ec141f93289b4596b1ee47bf9
deccodez <obarodayo@gmail.com>
2020-03-24 17:24:21 +0100

    my first initial commmit

---
+++ script.js
@@ -0,0 +1,14 @@
+var myName ="Obaro Oludayo Michael";
+var courses =['html', 'css', 'Javascript', 'php', 'golang', 'C#'];
+
+console.log(myName);
+console.log(courses);
+
+for (var i=0; i <= 220; i++)
+{
+    if(i % 2 ===0)
+    {
+        console.log(i);
+    }
+    
+}
